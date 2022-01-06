ce4f8ad89cff42fabc0749e3305287883d472658
soudegesu <soudegesu@hotmail.co.jp>
2022-01-06 09:36:09 +0900

    m97.4692.4.0.1にアップデート

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let file = "WebRTC-96.4664.2.0.1/WebRTC.xcframework.zip"
+let file = "WebRTC-97.4692.4.0.1/WebRTC.xcframework.zip"
 
 let package = Package(
     name: "Sora",
@@ -19,7 +19,7 @@ let package = Package(
         .binaryTarget(
             name: "WebRTC",
             url: "https://github.com/soudegesu/sora-ios-sdk-specs/releases/download/\(file)",
-            checksum: "eabcfe8ef79c45c09cbcbd9ce45759c72a5ff486e9fe6cc3b13935d45da769b9"),
+            checksum: "d1185616837fabe2eeef18d410e36c619b8d6f3a41922384b40b2279f95e8bb4"),
         .target(
             name: "Sora",
             dependencies: ["WebRTC", "Starscream"],
--- Podfile
+++ Podfile
@@ -5,7 +5,7 @@ platform :ios, '12.1'
 
 target 'Sora' do
   use_frameworks!
-  pod 'WebRTC', '96.4664.2.0.1'
+  pod 'WebRTC', '97.4692.4.0.1'
   pod 'Starscream', '3.1.1'
   pod 'SwiftLint'
   pod 'SwiftFormat/CLI'
